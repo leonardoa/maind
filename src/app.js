f5f26b8b7be840103d96da6ce6232e6d2e76aa95
Leonardo Angelucci <mail@leonardo-angelucci.ch>
2024-02-02 18:37:23 +0100

    data

--- src/app.js
+++ src/app.js
@@ -9,17 +9,24 @@ document.addEventListener("DOMContentLoaded", async () => {
     .on(
       "postgres_changes",
       { event: "*", schema: "public", table: "sensors" },
-      handleInserts
+      (payload) => {
+        handleInserts(payload.new);
+      }
     )
     .subscribe();
+
+  //select all data from sensors
+  let { data, error } = await database.from("sensors").select("*");
+  console.log(data);
+  handleInserts(data[0]);
 });
 
-function handleInserts(payload) {
+function handleInserts(data) {
   //variables
-  const x = payload.new.values.x;
-  const y = payload.new.values.y;
-  const time = payload.new.updated_at;
-  const id = payload.new.id;
+  const x = data.values.x;
+  const y = data.values.y;
+  const time = data.updated_at;
+  const id = data.id;
   //dom elements
   const contentX = document.getElementById("x");
   const contentY = document.getElementById("y");
@@ -38,29 +45,3 @@ document.addEventListener("mousemove", async (e) => {
     .update({ values: { x: e.clientX, y: e.clientY }, updated_at: new Date() })
     .eq("id", 123);
 });
-
-// const container = document.querySelector("#container");
-// const key =
-//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1bGdtbnhlb2hyc2l3a2hxamhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY4Nzk3NzcsImV4cCI6MjAyMjQ1NTc3N30.Z-F8w3dOtmnUXBtCBx07qw7AY86WCZzk7hFsKVeJejk";
-// const url = "https://gulgmnxeohrsiwkhqjhr.supabase.co";
-// const database = supabase.createClient(url, key);
-
-// //Document is ready
-// document.addEventListener("DOMContentLoaded", async () => {
-// 	// let res = await database.from("sensors").select("*");
-// 	// console.log(res);
-//   //display the data on the screen
-// //   container.innerHTML = `<h1> X: ${res.data[0].values.x} Y: ${res.data[0].values.y}</h1>`;
-
-//   //lets take the changes in realtime
-//   const subscription = database
-// 	.from("sensors")
-// 	.on("*", (payload) => {
-// 	  handleInserts(payload);
-// 	})
-// 	.subscribe();
-// });
-
-// const handleInserts = (payload) => {
-//   console.log("Change received!", payload);
-// };
